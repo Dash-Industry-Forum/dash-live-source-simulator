8e1b4964d3258d21ceb12ee73cc2e8ab631d0896
Armand Zangue <armand.zangue@gmail.com>
2016-08-03 17:47:51 +0200

    raise exception when too much drift is detected in the duration of the segments

--- dashlivesim/vodanalyzer/dashanalyzer.py
+++ dashlivesim/vodanalyzer/dashanalyzer.py
@@ -221,6 +221,8 @@ class DashAnalyzer(object):
                             rep_data['endTick'] = tfdt + duration
                             rep_data['endTime'] = rep_data['endTick']/float(track_timescale)
                             rep_data['endNr'] = segNr
+                        else:
+                            raise DashAnalyzerError("Too much drift in the duration of the segments")
                         segNr += 1
                         if self.verbose:
                             sys.stdout.write(".")
